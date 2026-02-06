7faac373ab57146a8bcaeb924784d95eb30c6019
Umair <umiiideveloper@gmail.com>
2026-02-06 13:35:19 +0500

    initial commit reffering empty structured folders

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='project which is use to predict the life expectancy in varous countries',
+    author='muhammad umair',
+    license='',
+)
